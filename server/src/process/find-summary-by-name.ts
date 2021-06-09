4a2d4ca0810eec64320e55d0a8830a9c5348ba9a
kkoemets <koemets.kristjan@gmail.com>
2021-06-09 19:24:26 +0300

    Add cache

--- server/src/process/find-summary-by-name.ts
+++ server/src/process/find-summary-by-name.ts
@@ -4,6 +4,10 @@ import { getLogger } from '../util/get-logger';
 import { findContract as findContractFromCoingecko } from './coingecko/coingecko-client';
 import { findSummary } from './find-summary';
 import { findContract as findContractFromCmc } from './coinmarketcap/coinmarketcap-client';
+import {
+  getCachedContractByName,
+  setCachedContractByName,
+} from './token-cache-manager';
 
 const log = getLogger();
 
@@ -13,11 +17,14 @@ export const findSummaryByName = async (
   log.info(`Finding for-${coinOfficialName}`);
 
   const contract =
+    (await getCachedContractByName({ coinOfficialName })) ||
     (await findContractFromCoingecko({ coinOfficialName })) ||
     (await findContractFromCmc({ coinOfficialName }));
   if (!contract) {
     return Promise.reject(Error('Could not find contract'));
   }
+
+  await setCachedContractByName({ coinOfficialName, contract });
 
   return await findSummary(contract);
 };
